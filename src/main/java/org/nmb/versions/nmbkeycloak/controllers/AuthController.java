3cd4d6780e8a28c80289b05c106207893243d9c2
Richard Ungani <richard.ungani@nmbbank.co.tz>
2024-07-25 18:24:14 +0300

    ..

--- src/main/java/org/nmb/versions/nmbkeycloak/controllers/AuthController.java
+++ src/main/java/org/nmb/versions/nmbkeycloak/controllers/AuthController.java
@@ -3,24 +3,25 @@ package org.nmb.versions.nmbkeycloak.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.nmb.versions.nmbkeycloak.dto.LoginRequestDto;
 import org.nmb.versions.nmbkeycloak.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/auth")
 public class AuthController {
 
-    @Autowired
-    private AuthService authService;
+    private final AuthService authService;
 
     @PostMapping(value = "/login")
-    public ResponseEntity<Object> login(@RequestBody LoginRequestDto request, HttpServletRequest servletRequest,
+    public ResponseEntity<Object> login(@RequestBody LoginRequestDto request,
+                                        HttpServletRequest servletRequest,
                                         HttpServletResponse servletResponse) {
         return authService.login(request, servletRequest, servletResponse);
     }
